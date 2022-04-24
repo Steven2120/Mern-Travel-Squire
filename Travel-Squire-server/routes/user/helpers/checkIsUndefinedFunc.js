f20a8df2e0c29486dd91de0f9b69308dca9efeba
Steven Moyano <stevenmoyano1@gmail.com>
2022-04-24 18:01:49 -0500

    created a function to catch if form items are left blank

--- Travel-Squire-server/routes/user/helpers/checkIsUndefinedFunc.js
+++ Travel-Squire-server/routes/user/helpers/checkIsUndefinedFunc.js
@@ -1,3 +1,11 @@
-function checkIsUndefinedFunc(req, res, next) {}
+function checkIsUndefined(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(500).json({ message: "Please fill out the form" });
+  } else {
+    let errorObj = {};
+    res.locals.errorObj = errorObj;
+    next();
+  }
+}
 
-module.exports = checkIsUndefinedFunc;
+module.exports = checkIsUndefined;
