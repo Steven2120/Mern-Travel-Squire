033abea9c8ecb51abcae0269735f6a79e3f97517
Steven Moyano <stevenmoyano1@gmail.com>
2022-04-26 12:03:20 -0500

    completed user routes. Will commence testing using postman

--- Travel-Squire-server/routes/user/userRouter.js
+++ Travel-Squire-server/routes/user/userRouter.js
@@ -8,4 +8,42 @@ const {
   getUserInfo,
 } = require("./controller/userController");
 
+const jwtMiddleware = require("../utils/jwtMiddleware");
+
+const checkIsUndefinedFunc = require("./helpers/checkIsUndefinedFunc");
+
+const checkIsStrongPasswordFunc = require("./helpers/checkIsStrongPasswordFunc");
+
+const checkIsEmptyFunc = require("./helpers/checkIsEmptyFunc");
+
+const {
+  checkIsEmailFunc,
+  checkIsAlphaFunc,
+  checkIsAlphanumericFunc,
+} = require("./helpers/authMiddleware");
+const app = require("../../app");
+
+router.post(
+  "/sign-up",
+  checkIsUndefinedFunc,
+  checkIsStrongPasswordFunc,
+  checkIsAlphanumericFunc,
+  checkIsAlphaFunc,
+  checkIsEmailFunc,
+  checkIsEmptyFunc,
+  createUser
+);
+
+router.post(
+  "/login",
+  checkIsUndefinedFunc,
+  checkIsEmptyFunc,
+  checkIsEmailFunc,
+  login
+);
+
+router.put("/update-user-profile", jwtMiddleware, updateUser);
+
+router.get("/get-user-info", jwtMiddleware, getUserInfo);
+
 module.exports = router;
